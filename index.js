6ac14508b3915655d1dcd12ddabf55098dd30c51
Brian Cook <bcrazorrider@gmail.com>
2020-06-26 18:08:43 -0400

    getting first question about title to display

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
+const generateMarkdown = require("./utils/generateMarkdown")
+
+// adding fs var
+const fs = require("fs")
+
+// adding inquirer var
+const inquirer = require("inquirer")
+
 // array of questions for user
-const questions = [
-
-];
-
+const questions = () => {
+  return inquirer.prompt([
+   {
+       type: "input",
+       name: "title",
+       message: "What is your Project Title?",
+   },
+  ])
+};
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
 function init() {
-
+console.log("Create a ReadMe!");
+questions()
 }
 
 // function call to initialize program
